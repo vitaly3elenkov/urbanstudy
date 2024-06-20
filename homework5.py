8042811176d47104888dc883e1c6ec5279909e95
vitaly3elenkov <zelenkovvitaly01@gmail.com>
2024-06-20 15:46:09 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,11 @@
+immutable_var=(1,2,"a,b,c")
+print("Кортеж:", immutable_var)
+#immutable_var[0]=1000 # кортеж - это неизменяемые упорядоченные коллекции элементов. Попытка изменения приведет к ошибке
+mutable_list=(10,9,8)
+print("Неизменяемый списк:",mutable_list )
+mutable_list1=([10,9],8)
+mutable_list1[0][0] =7
+print("Изменяемый списк:",mutable_list1)
+
+Author="Zelenkov Vitaly"
+print("ДЗ №5 выполнил:",Author)
